841a2fee2b716d1bfc2c89581d1e21fd6acc4e8f
Trent Turner <thturner19@gmail.com>
2018-10-23 11:19:50 -0400

    successful implementation of train schedules in info windows, finally able to find the user's location, still need to be able to recenter the map

--- mbta/mbta_script.js
+++ mbta/mbta_script.js
@@ -15,7 +15,7 @@ var redLineStops = [
   {name: "Andrew", stop_id: "place-andrw", lat: 42.330154, lng: -71.057655},
   {name: "JFK/UMass", stop_id: "place-jfk", lat: 42.320685, lng: -71.052391},
   //branches here
-  {name: "Saving Hill", stop_id: "place-shmnl", lat: 42.31129, lng: -71.053331},
+  {name: "Savin Hill", stop_id: "place-shmnl", lat: 42.31129, lng: -71.053331},
   {name: "Field Circle", stop_id: "place-fldcr", lat: 42.300093, lng: -71.061667},
   {name: "Shawmut", stop_id: "place-smmnl", lat: 42.29312583, lng: -71.06573796000001},
   {name: "Ashmont", stop_id: "place-asmnl", lat: 42.284652, lng: -71.06448899999999},
@@ -29,35 +29,50 @@ var redLineStops = [
 
 var testCoords = {latitude: 42.3113, longitude: -71.05332}
 
+var testLine = [
+  {latitude: 42.3113, longitude: -71.05332},
+  {latitude: 42.39674, longitude: -71.121815}
+];
+
 //producing an array with stops from alewife to ashmont
 var ashmontFullLine = redLineStops.slice(0, 17);
 //producing an array with the JFK/UMass stops and the Braintree branch stops
 var braintreeBranch = redLineStops.slice(17, 22);
 braintreeBranch.unshift(redLineStops[12]);
 
-//geolocation section. Unable to test the geolocation on a local server I think
-//Need to check with office hours or Ming
-function saveLocation(pos){
-  var coords = pos.coords;
-  console.log(coords);
-  console.log("v good");
-}
-function locationError(){}
-if (navigator.geolocation){
-  //console.log("good");
-  //var location = navigator.geolocation.getCurrentPosition(saveLocation, locationError);
-};
-
+var yourPos = {lat: NaN, lng: NaN};
 
 
 //Creating the display
 var map;
 function initMap () {
+  //Calling the function to find the nearest T station
+  //var location = findNearest(testCoords, redLineStops);
+
+  if (navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      yourPos.lat = pos.lat;
+      yourPos.lng = pos.lng;
+    });
+  };
+
+
+  //Calling the function to find the nearest T station
+  //var location = findNearest(yourPos, redLineStops);
+
   //making the map the background, initially centered at South Station
   map = new google.maps.Map(document.getElementById("map"), {
     center: redLineStops[9], //initially centered on south station
     zoom: 12
   });
+  /*if (yourPos.lat != NaN && yourPos.lng != NaN) {
+    map.setCenter(yourPos);
+  };*/
+
 
 //creating a polyline connecting the stops from alewife to ashmont
   var redLineMajor = new google.maps.Polyline ({
@@ -77,17 +92,18 @@ function initMap () {
     map: map,
   });
 
+  /*var personalLine = new google.maps.Polyline ({
+    path: testLine,
+    strokeColor: '#0000FF',
+    strokeOpactiy: 1.0,
+    strokeWidth: 3,
+    map:map,
+  })*/
 
-
-
-
-
-  var infoWindow = new google.maps.InfoWindow({
-    content: "ugh"
-  });
 
 //creating and placing the station markers on the map
   var stationMarker = [];
+  var stopId = [];
   for (i=0; i<redLineStops.length; i++) {
     stationMarker[i] = new google.maps.Marker({
       position: redLineStops[i],
@@ -99,21 +115,21 @@ function initMap () {
       map: map
     })
 
+    var infoWindow = new google.maps.InfoWindow({
+    });
+
+    stopId[i] = redLineStops[i].stop_id
+
     stationMarker[i].addListener('click', function() {
       infoWindow.open(map, this)
     });
 
-    stationMarker[i].addListener('click', function() {
+    stationMarker[i].addListener('click', function(stopId) {
       var index = stationMarker.indexOf(this);
-      var stopInfo = redLineStops[index];
-      fetchTrainInfo(stopInfo)
+      infoWindow.setContent(fetchTrainInfo(redLineStops[index].stop_id, redLineStops[index].name));
     });
-
   };
 
-
-  //Calling the function to find the nearest T station
-  //findNearest(testCoords, redLineStops);
 
 }
 
@@ -126,7 +142,7 @@ function toRadians(x) {
 //function to determine the closest station to you
 //requires two paramters, an object containing your latitude and longitute
 //and an aray of objects containing the stations' stop ids, lats, and longs
-/*function findNearest(yourLoc, stations) {
+function findNearest(yourLoc, stations) {
   var distance;
   var closestStop = {stop_id: "", distance: Infinity};
   var currentStation = new google.maps.LatLng(0, 0);
@@ -142,35 +158,75 @@ function toRadians(x) {
       closestStop.stop_id = stations[i].stop_id;
     }
   }
-}*/
+}
 
 
+
+
+function makeWindowContent(direction, arrival_time, name) {
+  var windowInfo = "<div id=info><h1>" + name + "</h1>"
+  + " <div class=direction>"
+  + "<p> Direction and time of the next 10 trains <p>";
+  for (count = 0; count < direction.length; count++) {
+    windowInfo += "<p>" + direction[count] + "    " + arrival_time[count] + "</p>";
+  };
+  windowInfo += "</div></div>";
+  return windowInfo;
+
+};
 
 
 //JSON Parsing for the stop information
-function fetchTrainInfo(stopInfo) {
-  var stopId = stopInfo.stop_id;
-
+function fetchTrainInfo(stopId, stopName) {
   var request = new XMLHttpRequest();
 
-  request.open("GET", "https://chicken-of-the-sea.herokuapp.com/redline/schedule.json?stop_id=" + stopId, true);
+  var contentString;
+  //request.open("GET", "https://chicken-of-the-sea.herokuapp.com/redline/schedule.json?stop_id=" + stopId, false)
+  request.open("GET", "https://api-v3.mbta.com/predictions?filter[route]=Red&filter[stop]=" + stopId + "&page[limit]=10&page[offset]=0&sort=departure_time&api_key=86e986351ed9401ba54ecce14c481d36", false);
 
-  request.onreadystatechange = function () {
+  request.onreadystatechange = function (trainInfo, direction, arrival_time) {
     if (request.readyState == 4 && request.status == 200) {
       var allData = request.responseText;
       var comingTrains = JSON.parse(allData);
+      var direction = [""];
+      var arrival_date = [];
+      var arrival_time = [];
+      var trainInfo = comingTrains.data; //data[+0-to-9+].attributes.(arrival_time, departure_time, direction_id)
+      for (count = 0; count < trainInfo.length; count++) {
+        if (comingTrains.data[count].attributes.direction_id == 1) {
+          direction[count] = "North";
+        } else {
+          direction[count] = "South"
+        };
+
+        arrival_date[count] = comingTrains.data[count].attributes.arrival_time;
+        arrival_time[count] = arrival_date[count].slice(11,19);
+      }
+
+      contentString = makeWindowContent(direction, arrival_time, stopName);
     }
     else if (request.readyState == 4 && request.status != 200) {
       alert("something went wrong")
     }
     else if (request.readyState == 3) {
-      alert("loading")
-    };
+
+    }
+
+  /*  for (count = 0; count < trainInfo.length; count++) {
+      if (comingTrains.data[count].attributes.direction_id == 1) {
+        direction[count] = "North (to Alewife)";
+      } else {
+        direction[count] = "South (to Ashmont/Braintree)"
+      };
+
+      arrival_time[count] = comingTrains.data[count].attributes.arrival_time;
+    }*/
+
+
+     //makeWindowContent(direction, arrival_time);
+
   };
 
   request.send();
-
-
-
-
-}
+  return contentString;
+};
